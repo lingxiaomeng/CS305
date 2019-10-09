aafdaae62b3067901458142bcbb805c349ed5b92
mlx <574243996@qq.com>
2019-10-09 23:38:16 +0800

    type detect

--- lab3/A2_asyncio.py
+++ lab3/A2_asyncio.py
@@ -5,22 +5,23 @@ from urllib import parse
 
 import chardet
 
-err405 = [b'HTTP/1.0 405 Not Found\r\n', b'Connection: close\r\n', b'Content-Type:text/html; charset=utf-8\r\n',
+err405 = [b'HTTP/1.0 405 Method Not Allowed\r\n', b'Connection: close\r\n',
+          b'Content-Type:text/html; charset=utf-8\r\n',
           b'\r\n',
-          b'<html><body>405 Not Found<body></html>\r\n', b'\r\n']
+          b'<html><body>405 Method Not Allowed<body></html>\r\n', b'\r\n']
 
 err404 = [b'HTTP/1.0 404 Not Found\r\n', b'Connection: close\r\n', b'Content-Type:text/html; charset=utf-8\r\n',
           b'\r\n',
           b'<html><body>404 Not Found<body></html>\r\n', b'\r\n']
 
 
-def get_dir_html(dir_path):
+def get_dir_html(dir_path):  # 请求为文件夹时
     body = '<html><head><title>Index of%s</title></head><body bgcolor="white"><h1>Index of%s</h1><hr><pre>' % (
         dir_path, dir_path)
     for file in os.listdir(dir_path):
         body += '<a href="%s">%s</a><br>' % (dir_path + '/' + file, file)
     body += '</pre><hr></body></html>'
-    content_length = 'Content-Length: %s\r\n' % str(len(body.encode()))
+    content_length = 'Content-Length: %s; charset=utf-8\r\n' % str(len(body.encode()))
     html = [b'HTTP/1.0 200 OK\r\n', b'Connection: close\r\n', b'Content-Type:text/html; charset=utf-8\r\n',
             content_length.encode(), b'\r\n',
             body.encode(),
@@ -28,14 +29,18 @@ def get_dir_html(dir_path):
     return html
 
 
-def get_file_html(file_path):
-    print(mimetypes.guess_type(file_path))  # 获取文件content-type
+def get_file_html(file_path):  # 请求为文件时
+    file_type = str(mimetypes.guess_type(file_path)[0])  # 获取文件content-type
     file = open(file_path, 'rb')
     data = file.read()
-    print(chardet.detect(data))
-    content_type = 'Content-Type: %s\r\n' % str(mimetypes.guess_type(file_path)[0])
+    if 'text' in file_type:
+        text_type = chardet.detect(data)['encoding']  # 判断文本文档的编码
+        content_type = 'Content-Type: %s; charset=%s\r\n' % (file_type, text_type)
+    else:
+        content_type = 'Content-Type: %s\r\n' % file_type
+
     content_length = 'Content-Length: %s\r\n' % str(os.path.getsize(file_path))
-    html = [b'HTTP/1.1 200 OK\r\n', b'Connection: close\r\n', content_type.encode(), content_length.encode(), b'\r\n',
+    html = [b'HTTP/1.0 200 OK\r\n', b'Connection: close\r\n', content_type.encode(), content_length.encode(), b'\r\n',
             data,
             b'\r\n']
     file.close()
